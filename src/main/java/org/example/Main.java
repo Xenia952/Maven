80d4e7af11c390930accd540a76e98fc3579efe6
Ksenia-laptop <xenasapoznikova@gmail.com>
2025-10-16 17:58:19 +0200

    lesson 2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,174 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+        printColor();
+        checkSumSign();
+        compareNumbers();
+        System.out.println(amountLimits(9, 12));
+        checkingTheNumbers(8);
+        System.out.println(checkingPositiveAndNegativeNumbers(-12));
+        printALine("вафля", 8);
+        System.out.println(leapYear(4000));
+        replaceTheNumbers();
+        fillInAnEmptyArray ();
+        multiplyingNumbersInAnArray();
+        fillInASquareArray();
+        createAnArray(4, 12);
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void  checkSumSign(){
+        int a = 5;
+        int b = 8;
+        if((a+b) >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor(){
+        int value = 150;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value <= 100) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a = 7;
+        int b = 8;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static boolean amountLimits(int a, int b){
+       int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void checkingTheNumbers(int a){
+        if (a >= 0){
+            System.out.println("Число положительное");
+        }
+        else {
+            System.out.println("Число отрицательное");
+        }
+
+    }
+    public static boolean checkingPositiveAndNegativeNumbers(int a){
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void printALine (String a, int b){
+
+       for (int i = 0; i < b; i++) {
+           System.out.println(a);
+       }
+
+    }
+
+    public static boolean leapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        else {
+            if (year % 4 == 0 && year % 100 != 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+    }
+
+    public static void replaceTheNumbers(){
+        int[] a = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 1) {
+                a[i] = 1;
+            }
+            else  {
+                a[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void fillInAnEmptyArray () {
+        int[] a = new int[100];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i+1;
+        }
+        System.out.println(Arrays.toString(a));
+
+    }
+
+    public static void multiplyingNumbersInAnArray(){
+        int [] a = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 6) {
+                a[i] = a[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void fillInASquareArray(){
+        int [][] a = new int [4][4];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (i == j) {
+                    a[i][j] = 1;
+                }
+                else {
+                    a[i][j] = 0;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(a));
+    }
+
+    public static void createAnArray (int len, int initialValue) {
+        int [] a = new int[len];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = initialValue;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
+
+
+
